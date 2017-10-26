ee18cc42c32189e1c21e85028f0a52aa0c6ebd89
Wendi Li <wendicodes@gmail.com>
2017-10-26 22:02:00 +0200

    gem files

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,14 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def index
+    @flats = Flat.where.not(latitude: nil, longitude: nil)
+
+    @hash = Gmaps4rails.build_markers(@flats) do |flat, marker|
+      marker.lat flat.latitude
+      marker.lng flat.longitude
+      # marker.infowindow render_to_string(partial: "/flats/map_box", locals: { flat: flat })
+    end
+  end
 end
